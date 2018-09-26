eb4a58b68ee4a92bfcc392cdf714e5d1d88fb363
Jim Disenstam <jim.disenstam@gmail.com>
2018-09-26 15:46:03 +0200

    Adding and testing median function

--- src/statistics.js
+++ src/statistics.js
@@ -22,6 +22,7 @@ function descriptiveStatistics (numbers) {
   console.log(minimum(numbers))
   console.log(range(numbers))
   console.log(mean(numbers))
+  console.log(median([6, 2, 3, 5, 6, 1, 7]))
 }
 
 function maximum (numbers) {
@@ -43,10 +44,21 @@ function mean (numbers) {
   let mean = numbers.reduce((a, b) => a + b, 0) / numbers.length
   return mean
 }
+
+function median (numbers) {
+  let sortedNumbers = numbers.slice(0).sort(function (a, b) { return a - b })
+  let begin = Math.round(sortedNumbers.length / 2) - 1
+  let end = begin + 2
+  if (sortedNumbers.length % 2) {
+    end = begin + 1
+  }
+  let median = mean(sortedNumbers.slice(begin, end))
+  return median
+}
 // Exports
 exports.descriptiveStatistics = descriptiveStatistics
 exports.maximum = maximum
-exports.mean = undefined
+exports.mean = mean
 exports.median = undefined
 exports.minimum = minimum
 exports.mode = undefined
